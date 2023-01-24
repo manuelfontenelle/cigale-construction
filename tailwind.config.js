6c72b0ea806922ef3d3288eb5602d946edae3548
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2023-01-24 15:48:14 +0100

    Slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ module.exports = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
-		"./src/**/*.{html,js}",
-		"./node_modules/tw-elements/dist/js/**/*.js",
+		// "./src/**/*.{html,js}",
+		// "./node_modules/tw-elements/dist/js/**/*.js",
 	],
 	theme: {
 		extend: {
