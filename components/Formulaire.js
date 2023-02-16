f664a0fb4009ddc11bf7b68a0da18ffb893849cc
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2023-02-16 22:04:02 +0100

    form final

--- components/Formulaire.js
+++ components/Formulaire.js
@@ -21,7 +21,8 @@ const Formulaire = () => {
 	// console.log(selectedFile)
 
 	const handleFileChange = (event) => {
-		console.log(event.target.files[0].size)
+		// console.log(event.target.files[0].size)
+		// limite poids 25mo
 		if (event.target.files[0].size < 24000000) {
 			setSelectedFile(event.target.files[0])
 			const alertMaxsize = document.querySelector("#alertMaxsize")
